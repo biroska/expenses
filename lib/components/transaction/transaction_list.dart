f0556b9be34a8eab69ee506177b9c5deab959f94
Aimberê Galdino <biroska@gmail.com>
2021-03-31 22:39:02 -0300

    Melhorando o codigo de controle de exibir imagem ou lista de transacoes

--- lib/components/transaction/transaction_list.dart
+++ lib/components/transaction/transaction_list.dart
@@ -16,71 +16,76 @@ class TransactionList extends StatelessWidget {
       // pre definido, senao o scroll nao sabe  calcular qdo realizar o scroll
 
       // O listView permite fazer loading da lista sob demanda
-      child:
-      transactions.isEmpty ?
+      child: transactions.isEmpty
+          ? showMessageEmptyTransactionList(context)
+          : showTransactionList(context),
+    );
+  }
 
-          Column(
-            children:  <Widget> [
-              // Adiciona espaco de 20 antes do componente
-              SizedBox( height: 30,),
-              Text(
-                'Nenhuma transação cadastrada',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox( height: 30,),
-              // Incluido container para que seja possivel delimitar o tamanho
-              // e assim podemos usar o BoxFit.cover
-              Container(
-                height:  200,
-                child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover ),
-              ),
-            ],
-          )
-          :
-      ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          // Mapeia cada elemento da lista para um widget Card e
-          // devolve uma lista pq estamos usando children
-          return Card(
-              child: Row(
-            children: <Widget>[
-              // Container do Preco
-              Container(
-                  // Espacamento interno ao container, mas o mais proximo da borda
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
+  ListView showTransactionList(BuildContext context) {
+    return ListView.builder(
+      itemCount: transactions.length,
+      itemBuilder: (ctx, index) {
+        final tr = transactions[index];
+        // Mapeia cada elemento da lista para um widget Card e
+        // devolve uma lista pq estamos usando children
+        return Card(
+            child: Row(
+          children: <Widget>[
+            // Container do Preco
+            Container(
+                // Espacamento interno ao container, mas o mais proximo da borda
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2)),
 
-                  // Similar ao padding do html, espacamento sobre o conteudo
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of( context ).primaryColor ),
-                  )),
-              // Container da Descricao e data
-              Container(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Text('${tr.title}',
-                      style: Theme.of(context).textTheme.headline6 ),
-                  Text(
-                    // Formata a data usando a biblioteca intl
-                    DateFormat('d MMM y').format(tr.date),
-                    style: TextStyle(color: Colors.grey[600]),
-                  )
-                ],
-              ))
-            ],
-          ));
-        },
-      ),
+                // Similar ao padding do html, espacamento sobre o conteudo
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor),
+                )),
+            // Container da Descricao e data
+            Container(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text('${tr.title}',
+                    style: Theme.of(context).textTheme.headline6),
+                Text(
+                  // Formata a data usando a biblioteca intl
+                  DateFormat('d MMM y').format(tr.date),
+                  style: TextStyle(color: Colors.grey[600]),
+                )
+              ],
+            ))
+          ],
+        ));
+      },
+    );
+  }
+
+  Column showMessageEmptyTransactionList(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        // Adiciona espaco de 20 antes do componente
+        SizedBox( height: 30 ),
+        Text(
+          'Nenhuma transação cadastrada',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        SizedBox( height: 30 ),
+        // Incluido container para que seja possivel delimitar o tamanho
+        // e assim podemos usar o BoxFit.cover
+        Container(
+          height: 200,
+          child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover),
+        ),
+      ],
     );
   }
 }
