1753f6ca66d761467644f7f1cb08f5c79dbc7147
Aimberê Galdino <biroska@gmail.com>
2021-08-23 18:28:25 -0300

    Ajuste tecnico para evitar overflow do teclado sobre os campos do formulario

--- lib/components/transaction/transaction_form.dart
+++ lib/components/transaction/transaction_form.dart
@@ -35,49 +35,53 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Título'),
-              controller: _titleController,
-              onSubmitted: (_) => _submitForm(),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Valor R\$'),
-              // No IOS o tipo number tras apenas numeros, por isso usamos numberWithOptions
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitForm(),
-              controller: _valueController,
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(_selectedDate == null
-                      ? 'Nenhuma data selecionada!'
-                      : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}'),
-                ),
-                TextButton(
-                    onPressed: _showDatePicker,
-                    child: Text(
-                      'Selecionar Data',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text('Nova Transação'),
-                )
-              ],
-            )
-          ],
+    // Envolvemos o formulario em um ScrollView e definimos o padding bottom,
+    // para evitar o overflow do teclado sobre os campos do fomulario
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.only(top:10, right: 10, left: 10, bottom: 10 + MediaQuery.of(context).viewInsets.bottom ),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Título'),
+                controller: _titleController,
+                onSubmitted: (_) => _submitForm(),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Valor R\$'),
+                // No IOS o tipo number tras apenas numeros, por isso usamos numberWithOptions
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitForm(),
+                controller: _valueController,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(_selectedDate == null
+                        ? 'Nenhuma data selecionada!'
+                        : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}'),
+                  ),
+                  TextButton(
+                      onPressed: _showDatePicker,
+                      child: Text(
+                        'Selecionar Data',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: Text('Nova Transação'),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
